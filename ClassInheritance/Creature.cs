d318eee024df5c353c66dc73cf8417aea9dd1b75
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-07-04 21:52:45 +0900

    C# - ClassInheritance, Interface, abstract
    Inheritance, Interface, abstract

---
+++ ClassInheritance/Creature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance
+{
+    internal abstract class Creature
+    {
+        public string DNA;
+        public int age;
+        public float mass;
+
+        public abstract void Breath();
+    }
+}
---
+++ ClassInheritance/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance
+{
+    internal class Dog : Creature, IFourLeggedWalker
+    {
+        public void FourLeggedWalk()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
