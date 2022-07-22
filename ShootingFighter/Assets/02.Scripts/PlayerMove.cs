48ad7559d2b98789532e082664b7ac3e4494eab6
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-07-22 21:52:53 +0900

    ShootingFighter - Rigidbody & Collider
    Created Player, PlayerMove, Enemy, Bullet to use Rigidbody & Collider

---
+++ ShootingFighter/Assets/02.Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Transform _tr;
+
+    private void Awake()
+    {
+        _tr = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        // Input.GetxxxRaw : 0 또는 1 출력
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v).normalized;
+        Vector3 deltaMove = dir * moveSpeed * Time.fixedDeltaTime;
+
+        _tr.Translate(deltaMove);
+    }
+}
