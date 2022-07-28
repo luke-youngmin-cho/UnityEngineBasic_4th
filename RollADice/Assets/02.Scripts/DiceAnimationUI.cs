859bc09c244a9de2a0c3d3959afa1041f15aecb1
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-07-28 21:53:14 +0900

    RollADice - Coroutine
    1. Coroutine
    - Usage of Unity coroutine (to implement dice rolling animation)
    - How Unity's coroutine works

---
+++ RollADice/Assets/02.Scripts/DiceAnimationUI.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+public class DiceAnimationUI : MonoBehaviour
+{
+    public static DiceAnimationUI instance;
+    [SerializeField] private Image _image;
+    [SerializeField] private float _animationDelay;
+    [SerializeField] private float _animationTime;
+    private float _timer;
+    private List<Sprite> sprites = new List<Sprite>();
+
+    private void Awake()
+    {
+        instance = this;
+        LoadSprites();    
+    }
+
+    private void LoadSprites()
+    {
+        sprites = Resources.LoadAll<Sprite>("DiceImages").ToList();        
+    }
+
+
+    //private class E_DiceAnimationEnum
+    //{
+    //    public int MoveNext()
+    //    {
+    //        int state = 0;
+    //        switch (state)
+    //        {
+    //            case 0:
+    //                if (GameManager.instance != null)
+    //                   {
+    //                         state++
+    //                         return 1;
+    //                    }
+    //                 else
+    //                      return -1;
+    //            case 1:
+    //                return 2;
+    //            case 2:
+    //                return 3;
+    //            default:
+    //                return -1;
+    //        }
+    //    }
+    //}
+
+    public void DoDiceAnimation()
+    {
+        StartCoroutine(E_DiceAnimation());
+    }
+
+    IEnumerator E_DiceAnimation()
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < _animationTime)
+        {
+            if (_timer < 0)
+            {
+                _image.sprite = sprites[Random.Range(0, sprites.Count)];
+                _timer = _animationDelay;
+            }
+            _timer -= Time.deltaTime;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        //while(elapsedTime < _animationTime)
+        //{
+        //    _image.sprite = sprites[Random.Range(0, sprites.Count)];
+        //    yield return new WaitForSeconds(_animationDelay);
+        //}
+        yield return null;
+    }
+}
