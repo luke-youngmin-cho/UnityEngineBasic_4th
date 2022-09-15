829861ca62602ccde8fb60f440d5aa4048194c2a
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-09-15 21:50:33 +0900

    TowerDefence - TowerHandler, GamePlay, LevelInfo..
    1. TowerHandler
    - Implemented mouse events
    
    2. GamePlay
    - Added just start logic
    
    3. LevelInfo
    - Created

---
+++ TowerDefence/Assets/02.Scripts/LevelInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelInfo", menuName = "TowerDefence/LevelInfo")]
+public class LevelInfo : ScriptableObject
+{
+    public int lifeInit;
+    public int moneyInit;
+}
