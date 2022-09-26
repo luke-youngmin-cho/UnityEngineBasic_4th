d085955e3e30ecfe955410ec9c56c843d902b844
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-09-26 21:58:21 +0900

    TowerDefence - Pathfinder
    Pathfinder
    - Completed BFS, DFS

--- TowerDefence/Assets/02.Scripts/Pathfinder.cs
+++ TowerDefence/Assets/02.Scripts/Pathfinder.cs
@@ -26,17 +26,24 @@ public class Pathfinder : MonoBehaviour
             => !(op1 == op2);
     }
 
-    enum Type
+    enum NodeType
     {
+        None,
         Path,
         Obstacle
+    }
+
+    public enum FindingOptions
+    {
+        BFS,
+        DFS
     }
 
     struct NodePair
     {
         public Transform node;
         public Coord coord;
-        public Type type;
+        public NodeType type;
     }
 
     private Transform _leftBottom; // (0, 0);
@@ -52,7 +59,7 @@ public class Pathfinder : MonoBehaviour
         { 0,-1, 0, 1 }
     };
     private List<List<Coord>> _pathList = new List<List<Coord>>();
-
+    private List<Coord> _tmpPathForDFS = new List<Coord>();
 
     public void SetNodeMap(List<Transform> pathNodes, List<Transform> obstacleNodes)
     {
@@ -78,7 +85,7 @@ public class Pathfinder : MonoBehaviour
             {
                 node = node,
                 coord = tmpCoord,
-                type = Type.Path
+                type = NodeType.Path
             };
         }
 
@@ -89,24 +96,57 @@ public class Pathfinder : MonoBehaviour
             {
                 node = node,
                 coord = tmpCoord,
-                type = Type.Obstacle
+                type = NodeType.Obstacle
             };
         }
     }
 
-    private bool BFS(NodePair start, NodePair end)
+    public void FindOptimizedPath(Transform startNode, Transform endNode, FindingOptions option)
     {
-        // 시작점과 끝점이 지날수 있는곳인지 체크
-        if (start.type != Type.Path ||
-            end.type != Type.Path)
-            return false;
+        bool found = false;
 
+        switch (option)
+        {
+            case FindingOptions.BFS:
+                found = BFS(start: FindNode(startNode).coord,
+                            end  : FindNode(endNode).coord);
+                break;
+            case FindingOptions.DFS:
+                found = DFS(start: FindNode(startNode).coord,
+                            end  : FindNode(endNode).coord);
+                break;
+            default:
+                break;
+        }
+
+        
+
+        if (found)
+        {
+            List<List<Transform>> nodePathList = new List<List<Transform>>();
+
+            for (int i = 0; i < _pathList.Count; i++)
+            {
+                string pathString = "";
+                nodePathList.Add(new List<Transform>());
+                for (int j = 0; j < _pathList[i].Count; j++)
+                {
+                    nodePathList[i].Add(GetNode(_pathList[i][j]));
+                    pathString += $"-> {nodePathList[i][j].name}";
+                }
+                Debug.Log($"[Pathfinder] : 최단경로 탐색 됨. {pathString}");
+            }
+        }
+    }
+
+    private bool BFS(Coord start, Coord end)
+    {
         bool isFinished = false;
         List<KeyValuePair<Coord,Coord>> parentPairs = new List<KeyValuePair<Coord, Coord>>();
         Queue<Coord> queue = new Queue<Coord>();
-        queue.Enqueue(start.coord);
-        parentPairs.Add(new KeyValuePair<Coord, Coord>(Coord.error, start.coord));
-        _visited[start.coord.y, start.coord.x] = true;
+        queue.Enqueue(start);
+        parentPairs.Add(new KeyValuePair<Coord, Coord>(Coord.error, start));
+        _visited[start.y, start.x] = true;
 
         int searchCount = 0;
         while (queue.Count > 0)
@@ -123,7 +163,7 @@ public class Pathfinder : MonoBehaviour
                     continue;
 
                 // 탐색 위치가 장애물일 경우
-                if (_map[next.y, next.x].type == Type.Obstacle)
+                if (_map[next.y, next.x].type == NodeType.Obstacle)
                     continue;
 
                 // 방문 여부
@@ -136,11 +176,11 @@ public class Pathfinder : MonoBehaviour
                 _visited[next.y, next.x] = true;
 
                 // 도착 체크
-                if (next.y == end.coord.y &&
-                    next.x == end.coord.x)
+                if (next.y == end.y &&
+                    next.x == end.x)
                 {
                     isFinished = true;
-                    _pathList.Add(CalcPath(parentPairs, start.coord, end.coord));
+                    _pathList.Add(CalcPath(parentPairs, start, end));
                 }
                 else
                 {
@@ -152,18 +192,68 @@ public class Pathfinder : MonoBehaviour
         return isFinished;
     }
 
+    private bool DFS(Coord start, Coord end)
+    {
+        _visited[start.y, start.x] = true;
+        _tmpPathForDFS.Add(start);
+        Coord next;
+        for (int i = 0; i < _direction.GetLength(1); i++)
+        {
+            next.y = start.y + _direction[0, i];
+            next.x = start.x + _direction[1, i];
+
+            // 탐색 위치가 맵을 벗어나는지
+            if ((next.y < 0 || next.y >= _map.GetLength(0))||
+                (next.x < 0 || next.x >= _map.GetLength(1)))
+                continue;
+
+            // 탐색 위치가 장애물일 경우
+            if (_map[next.y, next.x].type == NodeType.Obstacle)
+                continue;
+
+            // 방문 여부
+            if (_visited[next.y, next.x] == true)
+                continue;
+
+            // 도착 여부
+            if (next.y == end.y &&
+                next.x == end.x)
+            {
+                _pathList.Add(_tmpPathForDFS);
+                return true;
+            }
+            else
+            {
+                return DFS(next, end);
+            }
+        }
+
+        return false;
+    }
+
+
+    /// <summary>
+    /// 기록된 부모/자식 노드 페어의 리스트로 
+    /// 경로를 계산해서 반환해주는 함수
+    /// </summary>
+    /// <returns>계산된 경로</returns>
     private List<Coord> CalcPath(List<KeyValuePair<Coord, Coord>> parentPairs, Coord start, Coord end)
     {
         List<Coord> path = new List<Coord>();
-        Coord tmpCoord = parentPairs.Last().Value;
-        path.Add(tmpCoord);
+
+        Coord coord = parentPairs.Last().Value; // 젤 마지막 노드
+        path.Add(coord);
 
         int index = parentPairs.Count - 1;
         while (index > 0 &&
                parentPairs[index].Key != start)
         {
-
+            path.Add(parentPairs[index].Key);
+            index = parentPairs.FindLastIndex(pair => pair.Value == parentPairs[index].Key);
         }
+        path.Add(start);
+
+        path.Reverse();
 
         return path;
     }
@@ -173,6 +263,22 @@ public class Pathfinder : MonoBehaviour
     {
         return new Coord((int)((node.position.z - _leftBottom.position.z) / _nodeTerm),
                          (int)((node.position.x - _leftBottom.position.x) / _nodeTerm));
+    }
+
+    private NodePair FindNode(Transform node)
+    {
+        Coord coord = TransformToCoord(node);
+        for (int i = 0; i < _map.GetLength(1); i++)
+            for (int j = 0; j < _map.GetLength(0); j++)
+                if (_map[j,i].coord == coord)
+                    return _map[j,i];
+
+        return new NodePair() { coord = Coord.error, node = null, type = NodeType.None };
+    }
+
+    private Transform GetNode(Coord coord)
+    {
+        return _map[coord.y, coord.x].node;
     }
 
     private void Start()
