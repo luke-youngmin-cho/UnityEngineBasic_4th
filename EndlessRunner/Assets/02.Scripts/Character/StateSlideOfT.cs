d0adfb95c6dbc203e75b7a8bc89ce50144e8e0d2
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-10-26 21:53:22 +0900

    EndlessRunner - Slide, WallRun, URP Shader graph for MapUnit
    1. Slide, WallRun
    - Completed
    
    2. URP Shader graph for MapUnit
    - Created
    - Usage of shader graph

--- EndlessRunner/Assets/02.Scripts/Character/StateSlideOfT.cs
+++ EndlessRunner/Assets/02.Scripts/Character/StateSlideOfT.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using Unity.Animations.SpringBones.GameObjectExtensions;
+using UnityEngine;
+
 public class StateSlide<T> : StateBase<T> where T : Enum
 {
+    private CapsuleCollider _col;
+    private Vector3 _colCenterOrigin;
+    private float _colHeightOrigin;
+    private float _colShrinkRate = 0.25f;
     public StateSlide(StateMachineBase<T> stateMachine, T machineState, T canExecuteConditionMask, T nextTarget)
         : base(stateMachine, machineState, canExecuteConditionMask, nextTarget)
     {
+        _col = stateMachine.owner.gameObject.FindChildByName("Collision").GetComponent<CapsuleCollider>();
+        _colCenterOrigin = _col.center;
+        _colHeightOrigin = _col.height;
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+        _col.center = _colCenterOrigin * _colShrinkRate;
+        _col.height = _colHeightOrigin * _colShrinkRate;
     }
 
     public override T Update()
@@ -66,5 +83,7 @@ public class StateSlide<T> : StateBase<T> where T : Enum
     {
         base.Reset();
         animationManager.SetBool("DoSlide", false);
+        _col.center = _colCenterOrigin;
+        _col.height = _colHeightOrigin;
     }
 }
--- EndlessRunner/Assets/02.Scripts/Character/StateWallRunOfT.cs
+++ EndlessRunner/Assets/02.Scripts/Character/StateWallRunOfT.cs
@@ -1,8 +1,92 @@
 ﻿using System;
+using Unity.Animations.SpringBones.GameObjectExtensions;
+using UnityEngine;
+
 public class StateWallRun<T> : StateBase<T> where T : Enum
 {
+    private Rigidbody _rb;
+    private WallDetector _wallDetector_L;
+    private WallDetector _wallDetector_R;
     public StateWallRun(StateMachineBase<T> stateMachine, T machineState, T canExecuteConditionMask, T nextTarget) 
         : base(stateMachine, machineState, canExecuteConditionMask, nextTarget)
     {
+        _rb = stateMachine.owner.GetComponent<Rigidbody>();
+        _wallDetector_L = stateMachine.owner.gameObject.FindChildByName("WallDetector_L").GetComponent<WallDetector>();
+        _wallDetector_R = stateMachine.owner.gameObject.FindChildByName("WallDetector_R").GetComponent<WallDetector>();
+       
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+        _rb.isKinematic = true;
+    }
+
+    public override T Update()
+    {
+        T next = machineState;
+
+        switch (current)
+        {
+            case IState<T>.Commands.Idle:
+                break;
+            case IState<T>.Commands.Prepare:
+                {
+                    if (_wallDetector_L.isDetected)
+                        animationManager.SetBool("DoWallRunLeft", true);
+                    else if (_wallDetector_R.isDetected)
+                        animationManager.SetBool("DoWallRunRight", true);
+                    else
+                        throw new Exception("[StateWallRun<>] : Wall run detector has problem");
+
+                    MoveNext();
+                }
+                break;
+            case IState<T>.Commands.Casting:
+                {
+                    MoveNext();
+                }
+                break;
+            case IState<T>.Commands.WaitForCastingFinished:
+                {
+                    animationManager.SetBool("DoWallRunLeft", false);
+                    animationManager.SetBool("DoWallRunRight", false);
+                    MoveNext();
+                }
+                break;
+            case IState<T>.Commands.Action:
+                {
+                    MoveNext();
+                }
+                break;
+            case IState<T>.Commands.WaitForActionFinished:
+                {
+                    if (animationManager.GetNormalizedTime() > 0.9f)
+                        MoveNext();
+                }
+                break;
+            case IState<T>.Commands.Finish:
+                {
+                    MoveNext();
+                }
+                break;
+            case IState<T>.Commands.WaitForFinished:
+                {
+                    next = nextTarget;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return next;
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+        animationManager.SetBool("DoWallRunLeft", false);
+        animationManager.SetBool("DoWallRunRight", false);
+        _rb.isKinematic = false;
     }
 }
